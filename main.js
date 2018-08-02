02ee2d6ac032aa401c1aee6468c26014781397db
Nicholas Bates <nbatesnz@gmail.com>
2018-08-02 22:31:48 +1200

    added header

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    $(document).on("scroll", function() {
+        $("header, #headerBuffer").toggleClass("small", $(document).scrollTop() > 10);
+    });
+    
+    $("nav.desktop .link, nav.mobile ul .link").click(function() {
+        $(this).children("a")[0].click();
+    });
+    $("header #hamburger").click(function() {
+        $("nav.mobile").slideToggle();
+    });
+    $("body").click(function(event) {
+        if (!$(event.target).is(".mobile") && !$(event.target).parents(".mobile").length > 0 && $("nav.mobile").css("display") != "none") {
+            $("nav.mobile").slideUp();
+        }
+    });
+    
+    $(window).resize(function() {
+        if ($("nav.mobile").css("display") != "none") {
+            $("nav.mobile").slideUp();
+        }
+    });
+});
